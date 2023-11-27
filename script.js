c8698134e826d68b0c4ebdfda005b529e75bb14a
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2023-11-27 12:16:05 +0000

    add files

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const fruits = [ "apple", "orange", "kiwi", "watermelon"]
+let x;
+x = fruits.includes("apple");
+x = fruits.indexOf("apple");
+x = fruits.slice();
+console.log(fruits);
+
+
+
+const numbers =[1, 2, 3, 4, 5, 6];
+Number.splice(3, 0, "ABCD");
+console.log(numbers);
+
+numbers.splice(3, 1);
+console.log(numbers);
+
+ numbers.splice(2, 3);
+ console.log(numbers)
